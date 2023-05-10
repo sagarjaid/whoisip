5cf200cd11a09f6717a6d73a53a05b7787884583
sagarjaid <sagarjaid321@gmail.com>
2023-05-11 02:58:15 +0530

    Adsense added

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,14 @@ export default function App({ Component, pageProps }) {
         src={`https://www.googletagmanager.com/gtag/js?id=G-J76FXF2VER`}
         strategy="afterInteractive"
       />
+
+      <Script
+        async={true}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7096102876286946"
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+      />
+
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
